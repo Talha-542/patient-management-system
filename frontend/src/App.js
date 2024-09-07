addc6470acdf3d93f8d34fedf90bbf9bfa7da12b
Hassan Ali <hassanalibhatti648@gmail.com>
2024-09-07 10:48:52 +0500

    Counter and Footer Build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 export default function App() {
   return (
     <>
      <Home />
+     <Footer />
+
     </>
   )
 }
