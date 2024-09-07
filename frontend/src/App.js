5be90c1c121b24e97c39b0c4e3d33b6b357d44b5
Hassan Ali <hassanalibhatti648@gmail.com>
2024-09-07 10:23:09 +0500

    Makes the Banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from './pages/Home/Home';
 import './App.css';
 
 export default function App() {
   return (
-    <div>
-      Pateint Management System
-    </div>
+    <>
+     <Home />
+    </>
   )
 }
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import HeroSection from '../../components/HeroSection/HeroSection'
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
